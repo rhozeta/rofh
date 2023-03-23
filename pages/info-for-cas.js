62e9d7612328ee1a6280b5ab4fce90d7e4309718
rhozeta <nicholasmazzotta@gmail.com>
2023-03-23 16:49:57 -0400

    build and export

--- pages/info-for-cas.js
+++ pages/info-for-cas.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/About.module.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
